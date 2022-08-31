61656ff610261758bd3c05f3c2719056b09e30cc
ERLMKea <erlm@kea.dk>
2022-08-31 12:24:59 +0200

    Remove not used import

--- src/main/java/com/example/marsi/model/Foto.java
+++ src/main/java/com/example/marsi/model/Foto.java
@@ -1,14 +1,9 @@
 
 package com.example.marsi.model;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import javax.annotation.Generated;
 
-import com.fasterxml.jackson.annotation.JsonAnyGetter;
-import com.fasterxml.jackson.annotation.JsonAnySetter;
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
